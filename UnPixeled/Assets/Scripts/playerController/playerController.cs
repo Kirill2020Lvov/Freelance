9ed7e6c7efdb142833c972a61f1a245d84a8d1ea
Exiolite <kirkirlvov@gmail.com>
2020-10-01 23:51:32 +0300

    FirstDay
    Первые концепты и первый день разработки подошел к концу

---
+++ UnPixeled/Assets/Scripts/playerController/playerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] public float playerSpeed = 5;
+    Rigidbody playerRB;
+
+    private void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        playerRB.velocity = new Vector3 (moveX, playerRB.velocity.y, moveZ) * playerSpeed * Time.deltaTime;
+    }
+}
---
+++ UnPixeled/Assets/Scripts/playerController/playerHealthStats.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealthStats : MonoBehaviour
+{
+    public float health;
+    public float energy;
+
+    void Start()
+    {
+        health = 100;
+        energy = 100;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            healtHit(50);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health < 0)
+        {
+            
+        }
+    }
+
+    void healtHit (float ammount)
+    {
+        health -= ammount;
+    }
+}
